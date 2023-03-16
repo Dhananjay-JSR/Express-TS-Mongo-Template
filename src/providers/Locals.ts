39ea43e766d4fed8b978cf5cbbeee82e02c27025
Akshay Jain <akshayjain1999.aj18@gmail.com>
2023-03-16 15:19:42 +0530

    fix(locals): Better type safety with locals

--- src/providers/Locals.ts
+++ src/providers/Locals.ts
@@ -1,4 +1,3 @@
-import { Application } from 'express';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 
@@ -7,39 +6,25 @@ class Locals {
 	 * Makes env configs available for your app
 	 * throughout the app's runtime
 	 */
-	public static config(): any {
+	public static config(){
 		dotenv.config({ path: path.join(__dirname, '../../.env') });
 
-		const url = process.env.APP_URL || `http://localhost:${process.env.PORT || 3000}`;
-		const port = process.env.PORT || 3000;
+		const port = process.env.PORT;
 		const mongooseUrl = process.env.MONGODB_URL;
-		const maxUploadLimit = process.env.APP_MAX_UPLOAD_LIMIT || '50mb';
-		const maxParameterLimit = process.env.APP_MAX_PARAMETER_LIMIT || '50mb';
-		const name = process.env.APP_NAME || '<App_Name>';
 		const year = (new Date()).getFullYear();
-		const copyright = `Copyright ${year} ${name} | All Rights Reserved`;
-		const description = process.env.APP_DESCRIPTION || 'Here goes the app description';
+		const copyright = `Copyright Akshay Jain @ ${year} | All Rights Reserved`;
+		const description = process.env.APP_DESCRIPTION;
 		const isCORSEnabled = process.env.CORS_ENABLED || true;
-		const jwtExpiresIn = process.env.JWT_EXPIRES_IN || 3;
-		const apiPrefix = process.env.API_PREFIX || 'api';
-		const logDays = process.env.LOG_DAYS || 20;
-        const jwtSecretKey= process.env.JWT_SECRET_TOKEN_KEY
-        const frontend_url=process.env.FRONTEND_URL
+		if(!port){
+			console.error('Insufficient env params')
+			process.exit(1);
+		}
 		return {
-			apiPrefix,
 			copyright,
 			description,
 			isCORSEnabled,
-			jwtExpiresIn,
-			logDays,
-			maxUploadLimit,
-			maxParameterLimit,
 			mongooseUrl,
-			name,
 			port,
-			url,
-            jwtSecretKey,
-            frontend_url
 		};
 	}
 }
