a3cc2b741e391c49544669426cc4917baec02485
Dhananjay-JSR <ajaysenday@gmail.com>
2024-06-28 01:15:44 +0530

    feat:Type Safe ENV with zod

--- src/providers/Locals.ts
+++ src/providers/Locals.ts
@@ -1,6 +1,13 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
+import zod from "zod"
+import { envSchema } from './Validator';
 
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends zod.infer<typeof envSchema> {}
+	}
+}
 class Locals {
 	/**
 	 * Makes env configs available for your app
@@ -9,16 +16,15 @@ class Locals {
 	public static config(){
 		dotenv.config({ path: path.join(__dirname, '../../.env') });
 
+		// Parse the env variables
+		envSchema.parse(process.env);
+
 		const port = process.env.PORT;
 		const mongooseUrl = process.env.MONGODB_URL;
 		const year = (new Date()).getFullYear();
 		const copyright = `Copyright Akshay Jain @ ${year} | All Rights Reserved`;
 		const description = process.env.APP_DESCRIPTION;
 		const isCORSEnabled = process.env.CORS_ENABLED || true;
-		if(!port){
-			console.error('Insufficient env params')
-			process.exit(1);
-		}
 		return {
 			copyright,
 			description,
@@ -28,5 +34,6 @@ class Locals {
 		};
 	}
 }
+
 
 export default Locals;
